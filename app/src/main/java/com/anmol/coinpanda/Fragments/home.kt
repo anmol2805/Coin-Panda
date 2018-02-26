527bd5844e0b5012425d23402ae82418cd00a97b
anmol2805 <saxenaanmol28@gmail.com>
2018-02-26 19:27:06 +0530

    home fragment added

---
+++ app/src/main/java/com/anmol/coinpanda/Fragments/home.kt
@@ -0,0 +1,22 @@
+package com.anmol.coinpanda.Fragments
+
+import android.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.anmol.coinpanda.R
+
+/**
+ * Created by anmol on 2/26/2018.
+ */
+class home : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?,
+                              container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+        // Inflate the layout for this fragment
+        return inflater?.inflate(R.layout.home,
+                container, false)
+    }
+}
