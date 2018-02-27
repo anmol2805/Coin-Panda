cd3d60602d7ed195bf2bc69de171a62d7c9c8504
anmol2805 <saxenaanmol28@gmail.com>
2018-02-27 21:38:06 +0530

    notificastion icon left

--- app/src/main/java/com/anmol/coinpanda/Model/Coin.kt
+++ app/src/main/java/com/anmol/coinpanda/Model/Coin.kt
@@ -4,8 +4,8 @@ package com.anmol.coinpanda.Model
  * Created by anmol on 2/27/2018.
  */
 class Coin {
-    private var coinname:String? = null
-    private var coinnotify: Boolean = false
+    var coinname:String? = null
+    var coinnotify: Boolean = false
 
     constructor()
     constructor(coinname: String, coinnotify: Boolean) {
