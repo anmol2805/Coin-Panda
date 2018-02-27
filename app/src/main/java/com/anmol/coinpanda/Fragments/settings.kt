35a2c49b39637a44d0444edf6e74b42a615e9034
anmol2805 <saxenaanmol28@gmail.com>
2018-02-27 08:32:20 +0530

    settings fragment added

--- app/src/main/java/com/anmol/coinpanda/Fragments/settings.kt
+++ app/src/main/java/com/anmol/coinpanda/Fragments/settings.kt
@@ -10,13 +10,13 @@ import com.anmol.coinpanda.R
 /**
  * Created by anmol on 2/26/2018.
  */
-class home : Fragment() {
+class settings : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?,
                               container: ViewGroup?, savedInstanceState: Bundle?): View? {
         
         // Inflate the layout for this fragment
-        return inflater?.inflate(R.layout.home,
+        return inflater?.inflate(R.layout.settings,
                 container, false)
     }
 }
