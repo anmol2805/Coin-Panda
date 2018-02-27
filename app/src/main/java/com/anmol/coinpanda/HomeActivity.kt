1bd4a1721e6da3293435a199db071b557c8963fa
anmol2805 <saxenaanmol28@gmail.com>
2018-02-27 08:59:36 +0530

    fragment manager added

--- app/src/main/java/com/anmol/coinpanda/HomeActivity.kt
+++ app/src/main/java/com/anmol/coinpanda/HomeActivity.kt
@@ -1,8 +1,12 @@
 package com.anmol.coinpanda
 
+import android.app.Fragment
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import com.anmol.coinpanda.Fragments.dashboard
+import com.anmol.coinpanda.Fragments.home
+import com.anmol.coinpanda.Fragments.settings
 import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
@@ -10,15 +14,17 @@ class HomeActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-
+                setFragment(home())
                 //message.setText(R.string.title_home)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_dashboard -> {
+                setFragment(dashboard())
                 //message.setText(R.string.title_dashboard)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_notifications -> {
+                setFragment(settings())
                 //message.setText(R.string.title_notifications)
                 return@OnNavigationItemSelectedListener true
             }
@@ -26,10 +32,17 @@ class HomeActivity : AppCompatActivity() {
         false
     }
 
+
+    private fun setFragment(fragment: Fragment) {
+        val fragmentManager = fragmentManager
+        fragmentManager.beginTransaction().replace(R.id.content,fragment).commit()
+        fragmentManager.executePendingTransactions()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
+        setFragment(home())
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 }
