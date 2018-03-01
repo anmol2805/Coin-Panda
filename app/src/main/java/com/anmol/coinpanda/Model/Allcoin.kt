b8633657a8cea16215c03a9d1f152a8b3af93d99
anmol2805 <saxenaanmol28@gmail.com>
2018-03-01 18:33:13 +0530

    model set

---
+++ app/src/main/java/com/anmol/coinpanda/Model/Allcoin.kt
@@ -0,0 +1,14 @@
+package com.anmol.coinpanda.Model
+
+/**
+ * Created by anmol on 3/1/2018.
+ */
+class Allcoin{
+    var coinname:String? = null
+
+    constructor()
+    constructor(coinname: String?) {
+        this.coinname = coinname
+    }
+
+}
