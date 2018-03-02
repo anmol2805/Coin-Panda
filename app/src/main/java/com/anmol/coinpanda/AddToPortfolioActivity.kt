c5b80c5e6cb5ad47061a747eccb5efaf88f7cf2b
anmol2805 <saxenaanmol28@gmail.com>
2018-03-02 13:45:22 +0530

    portfolio started

--- app/src/main/java/com/anmol/coinpanda/AddToPortfolioActivity.kt
+++ app/src/main/java/com/anmol/coinpanda/AddToPortfolioActivity.kt
@@ -2,11 +2,15 @@ package com.anmol.coinpanda
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 
 class AddToPortfolioActivity : AppCompatActivity() {
+
+    private var addcoinrecycler:RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_to_portfolio)
+        addcoinrecycler = findViewById(R.id.addcoinrecycler)
     }
 }
