f3797dcf6a3c742510b9d32f92f340117280f72d
anmol2805 <saxenaanmol28@gmail.com>
2018-03-02 09:02:11 +0530

    tweets set

--- app/src/main/java/com/anmol/coinpanda/Model/Tweet.kt
+++ app/src/main/java/com/anmol/coinpanda/Model/Tweet.kt
@@ -7,7 +7,6 @@ class Tweet{
     var coin:String?=null
     var flag:Boolean = false
     var tid:String?=null
-    var keywords: Array<String>? = null
     var main:String?=null
     var ngram2:String?=null
     var polarity:Number? = null
@@ -15,11 +14,10 @@ class Tweet{
     var tweet:String?=null
 
     constructor()
-    constructor(coin: String?, flag: Boolean, tid: String?, keywords: Array<String>?, main: String?, ngram2: String?, polarity: Number?, subjectivity: Number?, tweet: String?) {
+    constructor(coin: String?, flag: Boolean, tid: String?, main: String?, ngram2: String?, polarity: Number?, subjectivity: Number?, tweet: String?) {
         this.coin = coin
         this.flag = flag
         this.tid = tid
-        this.keywords = keywords
         this.main = main
         this.ngram2 = ngram2
         this.polarity = polarity
--- app/src/main/java/com/anmol/coinpanda/TweetsActivity.kt
+++ app/src/main/java/com/anmol/coinpanda/TweetsActivity.kt
@@ -2,18 +2,40 @@ package com.anmol.coinpanda
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anmol.coinpanda.Model.Tweet
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 
 class TweetsActivity : AppCompatActivity() {
-
+    lateinit var tweets:MutableList<Tweet>
+    var db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tweets)
         val coin : String = intent.getStringExtra("coin")
         title = coin
+        tweets = ArrayList()
+
         loadtweets(coin)
     }
 
     private fun loadtweets(coin: String) {
-            
+        db.collection("supernode").document("coins").collection(coin).orderBy("timestamp",Query.Direction.DESCENDING)
+                .addSnapshotListener{documentSnapshot, e ->
+                    tweets.clear()
+                    for (doc in documentSnapshot.documents){
+                        System.out.println("tweet data:"+doc.data)
+                        val mcoin = doc.getString("coin")
+                        val mflag = doc.getBoolean("flag")
+                        val mtid = doc.getString("id")
+                        val mmain = doc.getString("main")
+                        val mngram2 = doc.getString("ngram2")
+                        val mpolarity = doc.get("polarity")
+                        val msubjectivity = doc.get("subjectivity")
+                        val mtweet = doc.getString("tweet")
+                        val tweet = Tweet(mcoin,mflag,mtid,mmain,mngram2, mpolarity as Number?, msubjectivity as Number?,mtweet)
+                        tweets.add(tweet)
+                    }
+                }
     }
 }
