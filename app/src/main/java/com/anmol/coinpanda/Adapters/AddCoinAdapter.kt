adf4fd0e43e11d19a7073cdca643b07056b1a47d
anmol2805 <saxenaanmol28@gmail.com>
2018-03-03 08:58:21 +0530

    layout changed

--- app/src/main/java/com/anmol/coinpanda/Adapters/AddCoinAdapter.kt
+++ app/src/main/java/com/anmol/coinpanda/Adapters/AddCoinAdapter.kt
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import com.anmol.coinpanda.Interfaces.ItemClickListener
@@ -12,6 +13,7 @@ import com.anmol.coinpanda.Model.Allcoin
 import com.anmol.coinpanda.Model.Coin
 import com.anmol.coinpanda.R
 import com.bumptech.glide.Glide
+import com.google.firebase.firestore.FirebaseFirestore
 
 /**
  * Created by anmol on 2/27/2018.
@@ -29,13 +31,27 @@ class AddCoinAdapter(internal var c: Context, internal var allcoins: List<Allcoi
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val coindata = allcoins[position]
         holder.mcoinname?.text = coindata.coinname
+        val db = FirebaseFirestore.getInstance()
+        db.collection("users").document("Z2ycXxL6GyvPS23NTuYk").collection("portfolio").addSnapshotListener{documentSnapshot, e ->
+            for(doc in documentSnapshot.documents){
+                if(doc.id.contains(coindata.coinname!!)){
+                    holder.addtoport?.visibility = View.GONE
+                    holder.btnstatus?.visibility = View.VISIBLE
+                }
+            }
+        }
+
 
     }
 
     inner class MyViewHolder(itemView: View, private val mitemClickListener: ItemClickListener):RecyclerView.ViewHolder(itemView), View.OnClickListener {
         var mcoinname:TextView? = null
+        var addtoport:Button?=null
+        var btnstatus:TextView?=null
         init {
             this.mcoinname = itemView.findViewById(R.id.namecoin)
+            this.addtoport = itemView.findViewById(R.id.addtoport)
+            this.btnstatus = itemView.findViewById(R.id.btnstatus)
             itemView.setOnClickListener(this)
         }
 
