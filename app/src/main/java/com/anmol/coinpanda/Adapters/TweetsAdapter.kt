efb8a71649c980036e1702c05cfd5c5500c3ace7
anmol2805 <saxenaanmol28@gmail.com>
2018-03-07 03:41:34 +0530

    little changes

--- app/src/main/java/com/anmol/coinpanda/Adapters/TweetsAdapter.kt
+++ app/src/main/java/com/anmol/coinpanda/Adapters/TweetsAdapter.kt
@@ -1,6 +1,7 @@
 package com.anmol.coinpanda.Adapters
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -13,6 +14,10 @@ import com.anmol.coinpanda.Model.Coin
 import com.anmol.coinpanda.Model.Tweet
 import com.anmol.coinpanda.R
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 /**
  * Created by anmol on 2/27/2018.
@@ -31,6 +36,34 @@ class TweetsAdapter(internal var c: Context, internal var tweets: List<Tweet>, p
         val coindata = tweets[position] 
         holder.mtweet?.text = coindata.tweet
         holder.mcoin?.text = coindata.coin
+        holder.coinname?.text = coindata.coin_symbol
+        val urlpng = "https://raw.githubusercontent.com/crypti/cryptocurrencies/master/images/"+tweets[position].coin+".png"
+        val urljpg = "https://raw.githubusercontent.com/crypti/cryptocurrencies/master/images/"+tweets[position].+".jpg"
+        val urljpeg = "https://raw.githubusercontent.com/crypti/cryptocurrencies/master/images/"+tweets[position].coinname+".jpeg"
+
+        nametext?.text = tweets[position].coin
+        Glide.with(c).load(urlpng).listener(object : RequestListener<Drawable> {
+            override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+
+                return false
+            }
+
+            override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                Glide.with(c).load(urljpg).listener(object : RequestListener<Drawable> {
+                    override fun onLoadFailed(e: GlideException?, model: Any?, target: Target<Drawable>?, isFirstResource: Boolean): Boolean {
+                        Glide.with(c).load(urljpeg).into(imageicon)
+                        return true
+                    }
+
+                    override fun onResourceReady(resource: Drawable?, model: Any?, target: Target<Drawable>?, dataSource: DataSource?, isFirstResource: Boolean): Boolean {
+                        return false
+                    }
+
+                }).into(imageicon)
+                return true
+            }
+
+        }).into(imageicon)
 
 
 
@@ -39,13 +72,17 @@ class TweetsAdapter(internal var c: Context, internal var tweets: List<Tweet>, p
     inner class MyViewHolder(itemView: View, private val mitemClickListener: ItemClickListener):RecyclerView.ViewHolder(itemView), View.OnClickListener {
         var mtweet:TextView?=null
         var mcoin:TextView?=null
+        var coinname:TextView?=null
+        var image:ImageView?=null
         init {
             this.mtweet = itemView.findViewById(R.id.tweet)
             this.mcoin = itemView.findViewById(R.id.coin)
+            this.coinname = itemView.findViewById(R.id.coinname)
+            this.image = itemView.findViewById(R.id.coinicon)
             itemView.setOnClickListener(this)
         }
 
-        override fun onClick(p0: View?) {
+        override fun onClick(position: View?) {
             mitemClickListener.onItemClick(this.adapterPosition)
         }
 
