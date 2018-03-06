8e614a00ece7852deda91ca360492361affdd486
anmol2805 <saxenaanmol28@gmail.com>
2018-03-06 23:44:32 +0530

    testing time

--- app/src/main/java/com/anmol/coinpanda/Fragments/dashboard.kt
+++ app/src/main/java/com/anmol/coinpanda/Fragments/dashboard.kt
@@ -11,6 +11,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Switch
+import com.anmol.coinpanda.Adapters.TweetsAdapter
 import com.anmol.coinpanda.Interfaces.ItemClickListener
 import com.anmol.coinpanda.Model.Allcoin
 import com.anmol.coinpanda.Model.Tweet
@@ -33,6 +34,7 @@ class dashboard : Fragment() {
     private lateinit var tweetselect:Switch
     lateinit var tweets : MutableList<Tweet>
     lateinit var itemClickListener : ItemClickListener
+
     var db = FirebaseFirestore.getInstance()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val vi = inflater.inflate(R.layout.dashboard, container, false)
@@ -90,7 +92,8 @@ class dashboard : Fragment() {
                     }
                     if(activity!=null){
                         if(!tweets.isEmpty()){
-
+                            val tweetsAdapter = TweetsAdapter(activity!!,tweets,itemClickListener)
+                            cointweetrecycler?.adapter = tweetsAdapter
                         }
                     }
         }
