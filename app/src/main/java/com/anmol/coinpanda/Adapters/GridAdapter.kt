4b8e6e6f79ec01a11652ac1cdc1f1f425239ed5f
anmol2805 <saxenaanmol28@gmail.com>
2018-03-06 13:19:27 +0530

    grid coinversio done

--- app/src/main/java/com/anmol/coinpanda/Adapters/GridAdapter.kt
+++ app/src/main/java/com/anmol/coinpanda/Adapters/GridAdapter.kt
@@ -4,11 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.BaseAdapter
-import android.widget.ImageView
-import android.widget.RelativeLayout
-import android.widget.VideoView
+import android.widget.*
 import com.anmol.coinpanda.Model.Allcoin
+import com.anmol.coinpanda.R
+import kotlinx.android.synthetic.main.coinlayout.view.*
 
 /**
  * Created by anmol on 12/28/2017.
@@ -29,10 +28,8 @@ class GridAdapter(private val ctx: Context, private val resource: Int, private v
     }
 
     private inner class ViewHolder {
-        internal var img: ImageView? = null
-        internal var pimg: ImageView? = null
-        internal var mvid: VideoView? = null
-        internal var vidlayout: RelativeLayout? = null
+        var coinnameall:TextView?=null
+
     }
 
     override fun getView(position: Int, view: View, viewGroup: ViewGroup): View? {
@@ -41,13 +38,14 @@ class GridAdapter(private val ctx: Context, private val resource: Int, private v
         if (row == null) {
             val inflater = ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
             row = inflater.inflate(resource, null)
+            holder.coinnameall = row!!.findViewById(R.id.namecoinall)
             //holder.img = row!!.findViewById(R.id.galleryimg) as ImageView
             //holder.pimg = row.findViewById(R.id.playicon) as ImageView
             row.tag = holder
         } else {
             holder = row.tag as ViewHolder
         }
-
+        holder.coinnameall?.text = allcoins[position].coinname
 
 
         return row
