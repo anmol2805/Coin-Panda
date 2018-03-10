c711855fabc702f478bfe87f56dc6d364ebfd1af
anmol2805 <saxenaanmol28@gmail.com>
2018-03-10 13:10:14 +0530

    settings set

--- app/src/main/java/com/anmol/coinpanda/Fragments/settings.kt
+++ app/src/main/java/com/anmol/coinpanda/Fragments/settings.kt
@@ -6,17 +6,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.anmol.coinpanda.R
 
 /**
  * Created by anmol on 2/26/2018.
  */
 class settings : Fragment() {
-
+    var telegram : Button? = null
+    var review : Button? = null
+    var request : Button? = null
+    var donate : Button? = null
+    var share : Button? = null
+    var help : Button? = null
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.settings,
+        val vi = inflater.inflate(R.layout.settings,
                 container, false)
+        telegram = vi.findViewById(R.id.telegram)
+        review = vi.findViewById(R.id.review)
+        request = vi.findViewById(R.id.request)
+        donate = vi.findViewById(R.id.donate)
+        share = vi.findViewById(R.id.share)
+        help = vi.findViewById(R.id.support)
+
+        return vi
     }
 }
