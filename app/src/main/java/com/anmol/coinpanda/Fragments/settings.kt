046076bf8bc8d042a420c8d900582ae0b0dfc78d
anmol2805 <saxenaanmol28@gmail.com>
2018-03-11 02:49:42 +0530

    review done

--- app/src/main/java/com/anmol/coinpanda/Fragments/settings.kt
+++ app/src/main/java/com/anmol/coinpanda/Fragments/settings.kt
@@ -9,6 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import com.anmol.coinpanda.R
+import android.content.ActivityNotFoundException
+import android.net.Uri
+
 
 /**
  * Created by anmol on 2/26/2018.
@@ -35,6 +38,22 @@ class settings : Fragment() {
             shareintent.type = "text/plain"
             shareintent.putExtra(Intent.EXTRA_TEXT,"https://play.google.com/store/apps/details?id=com.anmol.coinpanda")
             startActivity(Intent.createChooser(shareintent,"Share app via..."))
+
+        }
+        review?.setOnClickListener{
+            val uri = Uri.parse("market://details?id=" + "com.anmol.coinpanda")
+            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+            // To count with Play market backstack, After pressing back button,
+            // to taken back to our application, we need to add following flags to intent.
+            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
+                    Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+            try {
+                startActivity(goToMarket)
+            } catch (e: ActivityNotFoundException) {
+                startActivity(Intent(Intent.ACTION_VIEW,
+                        Uri.parse("http://play.google.com/store/apps/details?id=" + "com.anmol.coinpanda")))
+            }
 
         }
         return vi
