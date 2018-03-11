8327e60e01c33ebb08b20c0b6ed91a8d316f00c6
anmol2805 <saxenaanmol28@gmail.com>
2018-03-11 08:11:10 +0530

    all coins bookmark done

--- app/src/main/java/com/anmol/coinpanda/Fragments/mycoins.kt
+++ app/src/main/java/com/anmol/coinpanda/Fragments/mycoins.kt
@@ -2,15 +2,30 @@ package com.anmol.coinpanda.Fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import com.anmol.coinpanda.Interfaces.ItemClickListener
+import com.anmol.coinpanda.Model.Tweet
 import com.anmol.coinpanda.R
+import com.google.firebase.firestore.FirebaseFirestore
 
 /**
  * Created by anmol on 3/11/2018.
  */
 class mycoins : Fragment(){
+    private var cointweetrecycler: RecyclerView?=null
+
+    private lateinit var tweetselect: Switch
+    lateinit var tweets : MutableList<Tweet>
+    lateinit var itemClickListener : ItemClickListener
+    var sedit: EditText? = null
+    var srch: Button? = null
+    var db = FirebaseFirestore.getInstance()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val vi = inflater.inflate(R.layout.mycoins, container, false)
 
