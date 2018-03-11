a75ea27e48c7134c9b5587791843c6d6c80a51a7
anmol2805 <saxenaanmol28@gmail.com>
2018-03-11 18:24:35 +0530

    ico added

---
+++ app/src/main/java/com/anmol/coinpanda/Fragments/ico.kt
@@ -0,0 +1,18 @@
+package com.anmol.coinpanda.Fragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.anmol.coinpanda.R
+
+/**
+ * Created by anmol on 3/11/2018.
+ */
+class ico : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val vi = inflater.inflate(R.layout.allcoins, container, false)
+        return vi
+    }
+}
