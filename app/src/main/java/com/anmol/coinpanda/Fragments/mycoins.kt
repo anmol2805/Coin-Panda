241fce402f67d9bc2d23adb5e35e79edc7a29239
anmol2805 <saxenaanmol28@gmail.com>
2018-03-11 17:14:24 +0530

    finally success

--- app/src/main/java/com/anmol/coinpanda/Fragments/mycoins.kt
+++ app/src/main/java/com/anmol/coinpanda/Fragments/mycoins.kt
@@ -84,7 +84,7 @@ class mycoins : Fragment(){
         cal.add(Calendar.MONTH,-1)
         val stringtime = format.format(cal.time)
         val prevtime = Timestamp.valueOf(stringtime)
-        db.collection("Tweets").whereGreaterThanOrEqualTo("date",prevtime).orderBy("date", Query.Direction.DESCENDING).addSnapshotListener{ documentSnapshot, e->
+        db.collection("Tweets").whereGreaterThanOrEqualTo("date",prevtime).orderBy("date", Query.Direction.DESCENDING).addSnapshotListener{ ds1, e->
             tweets.clear()
             val mycoins :ArrayList<String> = ArrayList()
             db.collection("users").document("MhqeP5vqgdadnSodwzPo").collection("portfolio").addSnapshotListener{ds,er->
@@ -94,23 +94,27 @@ class mycoins : Fragment(){
                 mycoins.add(name)
             }
                 val bookmarks : ArrayList<String> = ArrayList()
-        db.collection("users").document("MhqeP5vqgdadnSodwzPo").collection("bookmarks").addSnapshotListener{documnentSnapshot,e->
+        db.collection("users").document("MhqeP5vqgdadnSodwzPo").collection("bookmarks").addSnapshotListener{ds2,e->
             bookmarks.clear()
-            for (doc in documnentSnapshot.documents){
+            for (doc in ds2.documents){
                 val tweetid = doc.id
                 bookmarks.add(tweetid)
             }
+            System.out.println("bookmarks$bookmarks")
             if(p0 == null){
-                for(doc in documentSnapshot.documents) {
+                for(doc in ds1.documents) {
                     var i = 0
                     var j = 0
                     var booked = false
                     while (j< bookmarks.size){
-                        if(doc.id.contains(bookmarks[i])){
+                        if(doc.id.contains(bookmarks[j])){
                             booked = true
+                            System.out.println("booked:$booked" + doc.id)
                         }
                         j++
                     }
+                    System.out.println("bookedstatus:$booked")
+
                     while (i < mycoins.size) {
 
                         if (doc.getString("coin_symbol").contains(mycoins[i])) {
@@ -121,7 +125,7 @@ class mycoins : Fragment(){
                             val url = doc.getString("url")
                             val keyword = doc.getString("keyword")
                             val dates = doc.getString("dates")
-
+                            System.out.println("logging$booked")
                             val tweet = Tweet(coin, coin_symbol, mtweet, url,keyword,id,booked,dates)
                             tweets.add(tweet)
                         }
@@ -130,6 +134,7 @@ class mycoins : Fragment(){
                 }
                 if(activity!=null){
                     if(!tweets.isEmpty()){
+                        System.out.println("logging:$tweets")
                         val tweetsAdapter = TweetsAdapter(activity!!,tweets,itemClickListener)
                         cointweetrecycler?.adapter = tweetsAdapter
                     }
@@ -137,7 +142,7 @@ class mycoins : Fragment(){
 
             }
             else{
-                for(doc in documentSnapshot.documents) {
+                for(doc in ds1.documents) {
                     var i = 0
                     var j = 0
                     var booked = false
@@ -157,7 +162,9 @@ class mycoins : Fragment(){
                             val url = doc.getString("url")
                             val keyword = doc.getString("keyword")
                             val dates = doc.getString("dates")
+
                             if (coin.toLowerCase().contains(p0) || coin_symbol.toLowerCase().contains(p0) || mtweet.toLowerCase().contains(p0) || keyword.toLowerCase().contains(p0) ||coin.toUpperCase().contains(p0) || coin_symbol.toUpperCase().contains(p0) || mtweet.toUpperCase().contains(p0) || keyword.toUpperCase().contains(p0)){
+                                System.out.println("logging$booked")
                                 val tweet = Tweet(coin, coin_symbol, mtweet, url,keyword,id,booked,dates)
                                 tweets.add(tweet)
                             }
