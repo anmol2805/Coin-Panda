826d62445a625d51b60d0de6c76a975de0eb330f
anmol2805 <saxenaanmol28@gmail.com>
2018-03-18 11:09:45 +0530

    splash activity added

--- app/src/main/java/com/anmol/coinpanda/SplashActivity.kt
+++ app/src/main/java/com/anmol/coinpanda/SplashActivity.kt
@@ -1,12 +1,28 @@
 package com.anmol.coinpanda
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
 
 class SplashActivity : AppCompatActivity() {
-
+    val auth = FirebaseAuth.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        if(auth!=null){
+            val handler = Handler()
+            handler.postDelayed({
+                startActivity(Intent(this,HomeActivity::class.java))
+            },3000)
+        }
+        else{
+            val handler = Handler()
+            handler.postDelayed({
+                startActivity(Intent(this,LoginActivity::class.java))
+            },3000)
+        }
+
     }
 }
