9e67129ac5dee196ff993d8a6b16d78a31c3aca6
anmol2805 <saxenaanmol28@gmail.com>
2018-03-19 09:56:20 +0530

    update set

--- app/src/main/java/com/anmol/coinpanda/HomeActivity.kt
+++ app/src/main/java/com/anmol/coinpanda/HomeActivity.kt
@@ -17,8 +17,10 @@ import android.R.attr.versionName
 import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.pm.PackageInfo
-
-
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 
 
 class HomeActivity : AppCompatActivity() {
@@ -69,39 +71,48 @@ class HomeActivity : AppCompatActivity() {
             startActivity(Intent(this,LoginActivity::class.java))
         }
         else{
-            val db = FirebaseFirestore.getInstance()
-            db.collection("appversion").document("versioninfo").get().addOnCompleteListener {task ->
-                val snapshot = task.result
-                val updateversion = snapshot.getString("version")
-                val pInfo = this.packageManager.getPackageInfo(packageName, 0)
-                val version = pInfo.versionName
-                if (version != updateversion){
-                    val dialog = AlertDialog.Builder(this)
-                            .setTitle("New version available")
-                            .setMessage("Please, update app to new version to continue our services")
-                            .setCancelable(false)
-                            .setPositiveButton("Update"
-                            ) { dialog, which -> val uri = Uri.parse("market://details?id=" + "com.anmol.coinpanda")
-                                val goToMarket = Intent(Intent.ACTION_VIEW, uri)
-                                // To count with Play market backstack, After pressing back button,
-                                // to taken back to our application, we need to add following flags to intent.
-                                goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
-                                        Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
-                                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
-                                try {
-                                    startActivity(goToMarket)
-                                } catch (e: ActivityNotFoundException) {
-                                    startActivity(Intent(Intent.ACTION_VIEW,
-                                            Uri.parse("http://play.google.com/store/apps/details?id=" + "com.anmol.coinpanda")))
-                                } }.create()
-                    dialog.show()
-                }
-                else{
-                    System.out.println("update not needed")
+            val databaseReference = FirebaseDatabase.getInstance().reference
+            databaseReference.root.addValueEventListener(object : ValueEventListener{
+                override fun onCancelled(p0: DatabaseError?) {
+
                 }
 
+                override fun onDataChange(p0: DataSnapshot?) {
+                    if(p0!!.child("vname").value!=null){
+                        val updateversion = p0.child("vname").value
+                        val pInfo = packageManager.getPackageInfo(packageName, 0)
+                        val version = pInfo.versionName.toString().trim()
+                        System.out.println("version $updateversion $version")
+                        if (version == updateversion){
+                            System.out.println("update not needed")
+                        }
+                        else{
+                            val dialog = AlertDialog.Builder(this@HomeActivity)
+                                    .setTitle("New version available")
+                                    .setMessage("Please, update app to new version to continue our services")
+                                    .setCancelable(false)
+                                    .setPositiveButton("Update"
+                                    ) { dialog, which -> val uri = Uri.parse("market://details?id=" + "com.anmol.coinpanda")
+                                        val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+                                        // To count with Play market backstack, After pressing back button,
+                                        // to taken back to our application, we need to add following flags to intent.
+                                        goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
+                                                Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                                                Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+                                        try {
+                                            startActivity(goToMarket)
+                                        } catch (e: ActivityNotFoundException) {
+                                            startActivity(Intent(Intent.ACTION_VIEW,
+                                                    Uri.parse("http://play.google.com/store/apps/details?id=" + "com.anmol.coinpanda")))
+                                        } }.create()
+                            dialog.show()
+                        }
 
-            }
+                    }
+                }
+
+            })
+
             setFragment(dashboard())
             navigation.selectedItemId = R.id.navigation_dashboard
             navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
@@ -111,39 +122,48 @@ class HomeActivity : AppCompatActivity() {
 
     override fun onPostResume() {
         super.onPostResume()
-        val db = FirebaseFirestore.getInstance()
-        db.collection("appversion").document("versioninfo").get().addOnCompleteListener {task ->
-            val snapshot = task.result
-            val updateversion = snapshot.getString("version")
-            val pInfo = this.packageManager.getPackageInfo(packageName, 0)
-            val version = pInfo.versionName
-            if (version != updateversion){
-                val dialog = AlertDialog.Builder(this)
-                        .setTitle("New version available")
-                        .setMessage("Please, update app to new version to continue our services")
-                        .setCancelable(false)
-                        .setPositiveButton("Update"
-                        ) { dialog, which -> val uri = Uri.parse("market://details?id=" + "com.anmol.coinpanda")
-                            val goToMarket = Intent(Intent.ACTION_VIEW, uri)
-                            // To count with Play market backstack, After pressing back button,
-                            // to taken back to our application, we need to add following flags to intent.
-                            goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
-                                    Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
-                                    Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
-                            try {
-                                startActivity(goToMarket)
-                            } catch (e: ActivityNotFoundException) {
-                                startActivity(Intent(Intent.ACTION_VIEW,
-                                        Uri.parse("http://play.google.com/store/apps/details?id=" + "com.anmol.coinpanda")))
-                            } }.create()
-                dialog.show()
-            }
-            else{
-                System.out.println("update not needed")
+        val databaseReference = FirebaseDatabase.getInstance().reference
+        databaseReference.root.addValueEventListener(object : ValueEventListener{
+            override fun onCancelled(p0: DatabaseError?) {
+
             }
 
+            override fun onDataChange(p0: DataSnapshot?) {
+                if(p0!!.child("vname").value!=null){
+                    val updateversion = p0.child("vname").value.toString().trim()
+                    val pInfo = packageManager.getPackageInfo(packageName, 0)
+                    val version = pInfo.versionName
+                    System.out.println("version $updateversion $version")
+                    if (version == updateversion){
+                        System.out.println("update not needed")
+                    }
+                    else{
+                        val dialog = AlertDialog.Builder(this@HomeActivity)
+                                .setTitle("New version available")
+                                .setMessage("Please, update app to new version to continue our services")
+                                .setCancelable(false)
+                                .setPositiveButton("Update"
+                                ) { dialog, which -> val uri = Uri.parse("market://details?id=" + "com.anmol.coinpanda")
+                                    val goToMarket = Intent(Intent.ACTION_VIEW, uri)
+                                    // To count with Play market backstack, After pressing back button,
+                                    // to taken back to our application, we need to add following flags to intent.
+                                    goToMarket.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
+                                            Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                                            Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
+                                    try {
+                                        startActivity(goToMarket)
+                                    } catch (e: ActivityNotFoundException) {
+                                        startActivity(Intent(Intent.ACTION_VIEW,
+                                                Uri.parse("http://play.google.com/store/apps/details?id=" + "com.anmol.coinpanda")))
+                                    } }.create()
+                        dialog.show()
 
-        }
+                    }
+
+                }
+            }
+
+        })
     }
 
 }
