d93d8fbcf5fdbcb6ac054fbbcb24301a747c635d
anmol2805 <saxenaanmol28@gmail.com>
2018-04-28 17:51:18 +0530

    tweetdbservice started

---
+++ app/src/main/java/com/anmol/coinpanda/Services/TweetsdbService.java
@@ -0,0 +1,17 @@
+package com.anmol.coinpanda.Services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+
+public class TweetsdbService extends IntentService {
+
+    public TweetsdbService() {
+        super("TweetsdbService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+}
