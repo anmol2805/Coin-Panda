472001eee8751dae27c5ec47cec671b22632bb26
anmol2805 <saxenaanmol28@gmail.com>
2018-05-05 09:09:39 +0530

    0 or 1 added

--- app/src/main/java/com/anmol/coinpanda/Model/Sqltweet.kt
+++ app/src/main/java/com/anmol/coinpanda/Model/Sqltweet.kt
@@ -9,8 +9,9 @@ class Sqltweet {
     var tweetid:String?=null
     var dates : String? = null
     var coinpage : String?=null
+    var mytweet: Int?=null
 
-    constructor(coin: String?, coin_symbol: String?, tweet: String?, url: String?, keyword: String?, tweetid: String?, dates: String?, coinpage: String?) {
+    constructor(coin: String?, coin_symbol: String?, tweet: String?, url: String?, keyword: String?, tweetid: String?, dates: String?, coinpage: String?, mytweet: Int?) {
         this.coin = coin
         this.coin_symbol = coin_symbol
         this.tweet = tweet
@@ -19,6 +20,7 @@ class Sqltweet {
         this.tweetid = tweetid
         this.dates = dates
         this.coinpage = coinpage
+        this.mytweet = mytweet
     }
 
     constructor()
--- app/src/main/java/com/anmol/coinpanda/Services/TweetsdbService.java
+++ app/src/main/java/com/anmol/coinpanda/Services/TweetsdbService.java
@@ -10,7 +10,9 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
+import com.anmol.coinpanda.Helper.Dbcoinshelper;
 import com.anmol.coinpanda.Helper.Dbhelper;
+import com.anmol.coinpanda.Model.Allcoin;
 import com.anmol.coinpanda.Model.Sqltweet;
 import com.anmol.coinpanda.Mysingleton;
 
@@ -38,6 +40,13 @@ public class TweetsdbService extends IntentService {
                     JSONArray jsonArray = response.getJSONArray("tweets");
                     List<Sqltweet> sqltweets = new ArrayList<>();
                     sqltweets.clear();
+                    Dbcoinshelper dcb = new Dbcoinshelper(getBaseContext());
+                    List<Allcoin> allcoins = new ArrayList<>();
+                    allcoins = dcb.readData();
+                    ArrayList<String> coins = new ArrayList<>();
+                    for(int i = 0;i<allcoins.size();i++){
+                        coins.add(allcoins.get(i).getCoinname());
+                    }
 
                     while (c<450){
                         JSONObject obj = jsonArray.getJSONObject(c);
@@ -49,7 +58,13 @@ public class TweetsdbService extends IntentService {
                         String keyword = obj.getString("keyword");
                         String dates = obj.getString("date");
                         String coinpage = obj.getString("coin_handle");
-                        Sqltweet sqltweet = new Sqltweet(coin,coin_symbol,mtweet,url,keyword,id,dates,coinpage);
+                        int mytweet = 0;
+                        for(int j=0;j<coins.size();j++){
+                            if(coins.get(j).equals(coin_symbol)){
+                                mytweet = 1;
+                            }
+                        }
+                        Sqltweet sqltweet = new Sqltweet(coin,coin_symbol,mtweet,url,keyword,id,dates,coinpage,mytweet);
                         Dbhelper db = new Dbhelper(getBaseContext());
                         db.insertData(sqltweet);
                         System.out.println("tweetno" + c);
