eb918a231ac5cebe2c8c2f08b4a9305dfe490b5b
anmol2805 <saxenaanmol28@gmail.com>
2018-05-09 12:05:29 +0530

    loading activity started

---
+++ app/src/main/java/com/anmol/coinpanda/LoadingActivity.kt
@@ -0,0 +1,12 @@
+package com.anmol.coinpanda
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class LoadingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loading)
+    }
+}
