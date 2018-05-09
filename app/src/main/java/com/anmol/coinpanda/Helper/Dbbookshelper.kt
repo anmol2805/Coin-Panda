e70589aa59f9d895c90b940188463536f2eb9988
anmol2805 <saxenaanmol28@gmail.com>
2018-05-09 12:47:38 +0530

    changing structure

---
+++ app/src/main/java/com/anmol/coinpanda/Helper/Dbbookshelper.kt
@@ -0,0 +1,86 @@
+package com.anmol.coinpanda.Helper
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteCantOpenDatabaseException
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteDatabase.openOrCreateDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.widget.Toast
+import com.anmol.coinpanda.Model.Allcoin
+import com.anmol.coinpanda.Model.Sqlcoin
+import com.anmol.coinpanda.Model.Sqltweet
+import com.anmol.coinpanda.Model.Tweet
+
+val DB_NAME = "coinsdb"
+val TB_NAME = "coins_table"
+val COIN = "coin"
+val COIN_SYMBOL = "coin_symbol"
+val COIN_HANDLE = "coin_handle"
+class Dbcoinshelper (context: Context):SQLiteOpenHelper(context, DB_NAME,null,1){
+
+    override fun onCreate(p0: SQLiteDatabase?) {
+        val createtable = "CREATE TABLE " + TB_NAME + " (" +
+                COIN + " VARCHAR(256)," +
+                COIN_SYMBOL + " VARCHAR(256) PRIMARY KEY NOT NULL UNIQUE," +
+                COIN_HANDLE + " VARCHAR(256))"
+
+        p0?.execSQL(createtable)
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        p0?.execSQL("DROP TABLE IF EXISTS $TB_NAME")
+        onCreate(p0)
+    }
+
+    fun insertData(sqlcoin: Sqlcoin){
+        try{
+        val db = this.writableDatabase
+        val cv = ContentValues()
+        cv.put(COIN,sqlcoin.coin)
+        cv.put(COIN_SYMBOL,sqlcoin.coin_symbol)
+        cv.put(COIN_HANDLE,sqlcoin.coinpage)
+
+            val result = db.insert(TB_NAME,null,cv)
+            if(result == (-1).toLong())
+                System.out.println("coinstatus is failed")
+            else
+                System.out.println("coinstatus is successs")
+        }catch (e:SQLiteCantOpenDatabaseException){
+            e.printStackTrace()
+        }
+
+    }
+    fun readData():MutableList<Allcoin>{
+        val allcoins : MutableList<Allcoin> = ArrayList()
+        try {
+            val db = this.readableDatabase
+            val query = "Select * from $TB_NAME"
+            val result = db.rawQuery(query,null)
+            if(result.moveToFirst()){
+                do{
+                    val mcoin = result.getString(result.getColumnIndex(COIN))
+                    val coin_symbol = result.getString(result.getColumnIndex(COIN_SYMBOL))
+                    val coinpage = result.getString(result.getColumnIndex(COIN_HANDLE))
+                    val allcoin = Allcoin(coin_symbol,mcoin,coinpage)
+                    allcoins.add(allcoin)
+                }while (result.moveToNext())
+            }
+            result.close()
+            db.close()
+
+        }
+        catch (e:SQLiteCantOpenDatabaseException){
+            e.printStackTrace()
+        }
+        return allcoins
+    }
+
+    fun deleteCoin(sqlcoin: Sqlcoin) {
+        val db = this.writableDatabase
+        db.delete(TB_NAME, "$COIN_SYMBOL = ?", arrayOf(sqlcoin.coin_symbol))
+        db.close()
+    }
+
+}
+
--- app/src/main/java/com/anmol/coinpanda/Services/BookmarksdbService.kt
+++ app/src/main/java/com/anmol/coinpanda/Services/BookmarksdbService.kt
@@ -1,45 +1,38 @@
-package com.anmol.coinpanda
+package com.anmol.coinpanda.Services
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
-import com.anmol.coinpanda.Helper.COL_ID
+import android.app.IntentService
+import android.content.Context
+import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
+import android.view.View
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.VolleyError
+import com.android.volley.toolbox.JsonObjectRequest
+import com.anmol.coinpanda.Fragments.coinslist
 import com.anmol.coinpanda.Helper.Dbcoinshelper
 import com.anmol.coinpanda.Helper.Dbhelper
-import com.anmol.coinpanda.Helper.TABLE_NAME
 import com.anmol.coinpanda.Model.Sqlcoin
+import com.anmol.coinpanda.Model.Sqltweet
+import com.anmol.coinpanda.Mysingleton
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import org.json.JSONException
+import org.json.JSONObject
+import java.util.ArrayList
 
-class LoadingActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loading)
-        val dcb = Dbcoinshelper(this)
-        val dtb = Dbhelper(this)
-        val data = dcb.readData()
-        val dataquery = "Select * from $TABLE_NAME ORDER BY $COL_ID DESC"
-        val tweetdata = dtb.readData(dataquery)
-        if(data.isEmpty()){
-            loadcoins(dcb)
-        }
-        else{
-            if(tweetdata.isEmpty()){
+class BookmarksdbService : IntentService("BookmarksdbService") {
 
-            }
-            else{
-
-            }
-        }
-    }
-
-    private fun loadcoins(dcb: Dbcoinshelper) {
+    override fun onHandleIntent(intent: Intent?) {
         val db = FirebaseFirestore.getInstance()
         val auth = FirebaseAuth.getInstance()
         db.collection("users").document(auth.currentUser!!.uid).collection("portfolio").get().addOnCompleteListener {
             task ->
             val documentSnapshot = task.result
+
             val s = documentSnapshot.size()
+            val dcb = Dbcoinshelper(baseContext)
             if(s!=0){
                 for(doc in documentSnapshot){
                     val coinname = doc.getString("coin_name")
@@ -48,10 +41,10 @@ class LoadingActivity : AppCompatActivity() {
                     val sqlcoin = Sqlcoin(coinname,coinsymbol,coinpage)
                     dcb.insertData(sqlcoin)
                 }
+
+
             }
 
         }
     }
-
-
 }
