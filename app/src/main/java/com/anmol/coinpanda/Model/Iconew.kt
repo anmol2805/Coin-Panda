04f623d9a8a31a4b7b7f4dc198d8c2969beea737
anmol2805 <saxenaanmol28@gmail.com>
2018-06-11 00:32:13 +0530

    ico model added

---
+++ app/src/main/java/com/anmol/coinpanda/Model/Iconew.kt
@@ -0,0 +1,13 @@
+package com.anmol.coinpanda.Model
+
+class Iconew{
+    var iconame:String?=null
+    var newsdate:String?=null
+    var news:String?=null
+    constructor()
+    constructor(iconame: String?, newsdate: String?, news: String?) {
+        this.iconame = iconame
+        this.newsdate = newsdate
+        this.news = news
+    }
+}
