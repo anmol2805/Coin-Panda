e22d70182bd1f1d4505bae476b71841fee274af5
anmol2805 <saxenaanmol28@gmail.com>
2018-06-15 03:13:53 +0530

    build apk and testing

--- app/src/main/java/com/anmol/coinpanda/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/anmol/coinpanda/MyFirebaseMessagingService.kt
@@ -9,6 +9,7 @@ import android.media.RingtoneManager
 import android.net.Uri
 import android.support.v4.app.NotificationCompat
 import android.support.v4.app.TaskStackBuilder
+import android.widget.RemoteViews
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -27,15 +28,22 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     private fun showNotification(payload: Map<String, String>) {
         val alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val builder = NotificationCompat.Builder(this)
-
         builder.setSmallIcon(R.drawable.alpha)
         builder.setBadgeIconType(R.drawable.alpha)
         builder.color = 255
         builder.setWhen(System.currentTimeMillis())
-        builder.setContentTitle(payload["title"])
-        builder.setContentText(payload["body"])
+//        builder.setContentTitle(payload["title"])
+//        builder.setContentText(payload["body"])
         builder.setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
         builder.setSound(alarmSound)
+        // Get the layouts to use in the custom notification
+        val notificationLayout = RemoteViews(packageName, R.layout.notification_collapsed)
+        notificationLayout.setTextViewText(R.id.notification_title,payload["title"])
+        notificationLayout.setTextViewText(R.id.notification_info,payload["body"])
+        notificationLayout.setImageViewResource(R.id.coinnotifyicon,R.drawable.alpha)
+        builder.setStyle(NotificationCompat.DecoratedCustomViewStyle())
+        builder.setCustomContentView(notificationLayout)
+
         val resultIntent = Intent(this, Home2Activity::class.java)
         val stackbuilder = TaskStackBuilder.create(this)
         stackbuilder.addNextIntent(resultIntent)
