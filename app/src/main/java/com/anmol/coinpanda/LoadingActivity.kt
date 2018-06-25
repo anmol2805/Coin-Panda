f7b10cc123b787c0ed061401a1c9978aa3298bb2
anmol2805 <saxenaanmol28@gmail.com>
2018-06-26 02:35:28 +0530

    loading activity done

--- app/src/main/java/com/anmol/coinpanda/LoadingActivity.kt
+++ app/src/main/java/com/anmol/coinpanda/LoadingActivity.kt
@@ -18,6 +18,10 @@ import com.anmol.coinpanda.Model.Value
 import com.anmol.coinpanda.Services.BookmarksdbService
 import com.anmol.coinpanda.Services.TweetsdbService
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.messaging.FirebaseMessaging
 import org.json.JSONException
@@ -27,9 +31,9 @@ import java.util.ArrayList
 import java.util.HashMap
 
 class LoadingActivity : AppCompatActivity() {
-    var retry:Button?=null
-    var loadpgr:ProgressBar?=null
-    var pw:TextView?=null
+    var retry: Button? = null
+    var loadpgr: ProgressBar? = null
+    var pw: TextView? = null
     var db = FirebaseFirestore.getInstance()
     val auth = FirebaseAuth.getInstance()
     val messaging = FirebaseMessaging.getInstance()
@@ -41,37 +45,39 @@ class LoadingActivity : AppCompatActivity() {
         pw = findViewById<TextView>(R.id.pw)
         pw?.text = "Welcome to Cryptohype...!!!"
         val auth = FirebaseAuth.getInstance()
-        if(auth.currentUser == null){
-            startActivity(Intent(this,LoginActivity::class.java))
-        }
-        else{
+        if (auth.currentUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        } else {
             val databse = Dbtopicshelper(this)
-            val value = Value("secret",0)
+            val value = Value("secret", 0)
             databse.insertData(value)
             val datavalue = databse.readData()
             System.out.println("procedure data" + datavalue[0].keyvalue)
-            if(datavalue[0].keyvalue == 0){
+            if (datavalue[0].keyvalue == 0) {
                 System.out.println("procedure 0")
-                val db = FirebaseFirestore.getInstance()
-                db.collection("users").document(auth.currentUser!!.uid).collection("portfolio").get().addOnCompleteListener {
-                    task ->
-                    val documentSnapshot = task.result
-                    val s = documentSnapshot.size()
-                    if(s!=0){
-                        for(doc in documentSnapshot){
-                            val coinname = doc.getString("coin_name")
-                            val coinsymbol = doc.id
-                            topicsearch(0,coinsymbol,coinname)
-                        }
-                        val valuenew = Value("secret",1)
-                        databse.updatedata(valuenew)
+                val databaseReference = FirebaseDatabase.getInstance().reference
+                databaseReference.child("database").child(auth.currentUser!!.uid).child("portfolio").addListenerForSingleValueEvent(object : ValueEventListener {
+                    override fun onCancelled(p0: DatabaseError?) {
 
                     }
 
+                    override fun onDataChange(p0: DataSnapshot?) {
+                        if (p0!!.exists()) {
+                            for (data in p0.children) {
+                                val coinname = data.child("coin").value.toString()
+                                val coinsymbol = data.child("coin_symbol").value.toString()
+                                topicsearch(0, coinsymbol, coinname)
 
-                }
-            }
-            else{
+                            }
+                            val valuenew = Value("secret", 1)
+                            databse.updatedata(valuenew)
+
+                        }
+                    }
+
+                })
+
+            } else {
                 System.out.println("procedure 1")
             }
             startloading()
@@ -98,44 +104,45 @@ class LoadingActivity : AppCompatActivity() {
         val tweetdata = dtb.readData(dataquery)
         val bookmarkdata = dbb.readbook()
         pw?.text = "Please Wait!!!"
-        if(data.isEmpty()){
+        if (data.isEmpty()) {
             val db = FirebaseFirestore.getInstance()
             val auth = FirebaseAuth.getInstance()
-            db.collection("users").document(auth.currentUser!!.uid).collection("portfolio").get().addOnCompleteListener {
-                task ->
-                pw?.text = "Please Wait!!!"
-                val documentSnapshot = task.result
-                val s = documentSnapshot.size()
-                if(s!=0){
-                    for(doc in documentSnapshot){
-                        val coinname = doc.getString("coin_name")
-                        val coinsymbol = doc.id
-                        val coinpage = doc.getString("coinPage")
-                        topicsearch(0,coinsymbol,coinname)
-                        val sqlcoin = Sqlcoin(coinname,coinsymbol,coinpage)
-                        dcb.insertData(sqlcoin)
+            val databaseReference = FirebaseDatabase.getInstance().reference
+            databaseReference.child("database").child(auth.currentUser!!.uid).child("portfolio").addListenerForSingleValueEvent(object:ValueEventListener{
+                override fun onCancelled(p0: DatabaseError?) {
+                    retry?.visibility = View.VISIBLE
+                    loadpgr?.visibility = View.GONE
+                    pw?.text = "Network Error"
+                }
+
+                override fun onDataChange(p0: DataSnapshot?) {
+                    pw?.text = "Please Wait!!!"
+                    if(p0!!.exists()){
+                        for (doc in p0.children){
+                            val coinname = doc.child("coin").value.toString()
+                            val coinsymbol = doc.child("coin_symbol").value.toString()
+                            val coinpage = doc.child("coinpage").value.toString()
+                            topicsearch(0, coinsymbol, coinname)
+                            val sqlcoin = Sqlcoin(coinname, coinsymbol, coinpage)
+                            dcb.insertData(sqlcoin)
+                        }
+                        startloading()
                     }
-                    startloading()
-
-                }
-                else{
-                    val intent = Intent(this,TweetsdbService::class.java)
-                    startService(intent)
-                    val intent2 = Intent(this,Home2Activity::class.java)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                    startActivity(intent2)
-                    finish()
+                    else{
+                        val intent = Intent(this@LoadingActivity, TweetsdbService::class.java)
+                        startService(intent)
+                        val intent2 = Intent(this@LoadingActivity, Home2Activity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                        startActivity(intent2)
+                        finish()
+                    }
                 }
 
-            }.addOnFailureListener {
-                retry?.visibility = View.VISIBLE
-                loadpgr?.visibility = View.GONE
-                pw?.text = "Network Error"
-            }
-        }
-        else{
-            if(tweetdata.isEmpty()){
+            })
+
+        } else {
+            if (tweetdata.isEmpty()) {
                 pw?.text = "We're setting up few things for you..."
                 val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, "http://198.199.90.139/tweets", null, Response.Listener { response ->
                     pw?.text = "We're almost done...!"
@@ -163,7 +170,7 @@ class LoadingActivity : AppCompatActivity() {
                             println("tweetno$c")
                             c++
                         }
-                        val intent = Intent(this,Home2Activity::class.java)
+                        val intent = Intent(this, Home2Activity::class.java)
                         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                         startActivity(intent)
@@ -178,9 +185,8 @@ class LoadingActivity : AppCompatActivity() {
                     pw?.text = "Network Error"
                 })
                 Mysingleton.getInstance(baseContext).addToRequestqueue(jsonObjectRequest)
-            }
-            else{
-                val intent = Intent(this,Home2Activity::class.java)
+            } else {
+                val intent = Intent(this, Home2Activity::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
                 startActivity(intent)
@@ -189,45 +195,49 @@ class LoadingActivity : AppCompatActivity() {
             }
         }
     }
+
     private fun topicsearch(i: Int, coinname: String?, coin: String?) {
-        System.out.println("procedure topicsearch")
-        db.collection("topics").document(coinname + i.toString()).get().addOnCompleteListener { task->
-            val documentSnapshot = task.result
-            if(documentSnapshot.exists()){
-                val count = documentSnapshot.getLong("count")
-                if(count!! > 990){
-                    topicsearch(i+1, coinname, coin)
-                }
-                else{
-                    val map  = HashMap<String,Any>()
+        val databaseReference = FirebaseDatabase.getInstance().reference
+        databaseReference!!.child("topics").child(coinname + i.toString()).addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onCancelled(p0: DatabaseError?) {
+
+            }
+
+            override fun onDataChange(p0: DataSnapshot?) {
+                if (p0!!.exists()) {
+                    val count: Long = p0.child("count").value as Long
+                    if (count > 990) {
+                        topicsearch(i + 1, coinname, coin)
+                    } else {
+                        val map = HashMap<String, Any>()
+                        map["notify"] = true
+                        map["coinname"] = coin!!
+                        databaseReference.child("database").child(auth.currentUser!!.uid).child("topics").child(coinname + i.toString()).setValue(map)
+                                .addOnCompleteListener {
+                                    messaging.subscribeToTopic(coinname + i.toString())
+                                    val countmap = HashMap<String, Any>()
+                                    countmap["count"] = count + 1
+                                    countmap["coin_symbol"] = coinname.toString()
+                                    databaseReference.child("topics").child(coinname + i.toString()).setValue(countmap)
+                                }
+                    }
+                } else {
+                    val map = HashMap<String, Any>()
                     map["notify"] = true
                     map["coinname"] = coin!!
-                    db.collection("users").document(auth.currentUser!!.uid).collection("topics").document(coinname + i.toString())
-                            .set(map).addOnSuccessListener {
+                    databaseReference.child("database").child(auth.currentUser!!.uid).child("topics").child(coinname + i.toString())
+                            .setValue(map).addOnSuccessListener {
                                 messaging.subscribeToTopic(coinname + i.toString())
-                                val countmap = HashMap<String,Any>()
-                                countmap["count"] = count+1
-                                countmap["coin_symbol"] = coinname.toString()
-                                db.collection("topics").document(coinname + i.toString()).set(countmap)
+                                val count = HashMap<String, Any>()
+                                count["count"] = 1
+                                count["coin_symbol"] = coinname.toString()
+                                databaseReference.child("topics").child(coinname + i.toString()).setValue(count)
                             }
                 }
             }
-            else{
-                val map  = HashMap<String,Any>()
-                map["notify"] = true
-                map["coinname"] = coin!!
-                db.collection("users").document(auth.currentUser!!.uid).collection("topics").document(coinname + i.toString())
-                        .set(map).addOnSuccessListener {
-                            messaging.subscribeToTopic(coinname + i.toString())
-                            val count = HashMap<String,Any>()
-                            count["count"] = 1
-                            count["coin_symbol"] = coinname.toString()
-                            db.collection("topics").document(coinname + i.toString()).set(count)
-                        }
-            }
 
-        }
+        })
+
+
     }
-
-
 }
